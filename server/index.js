aa2816f72babfd0c729183177647067d032b1c2f
gugohh <oohhgug@gmail.com>
2021-11-22 20:45:08 +0100

    w

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://gugo:1234@cluster0.lthpa.mongodb.net/test?authSource=admin&replicaSet=atlas-ieyktw-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
+
+
+
 
 app.listen(3001, ()=>{
-    console.log("Server Runs Perfectly");
+    console.log("Server Runs Perfectly!");
 })
