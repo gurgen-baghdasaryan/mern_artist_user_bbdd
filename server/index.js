ccae914a8ed972473f2d26f4bb5c0128b6c631d5
gugohh <oohhgug@gmail.com>
2021-11-22 20:15:03 +0100

    working with Server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+const express = require("express")
+const app = express()
+
+app.listen(3001, ()=>{
+    console.log("Server Runs Perfectly");
+})
