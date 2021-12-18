a9f28c0a957248e4a633c8199a29702d8dc61a89
gugohh <oohhgug@gmail.com>
2021-12-18 14:44:24 +0100

    Data Import Success

---
+++ server/data/artists.js
@@ -0,0 +1,53 @@
+const artists = [
+  {
+    name: "Nadiner",
+    imageUrl:
+      "https://nadinefundacion.org/wp-content/uploads/2021/07/DSC04413.jpg",
+    description:
+      "Memento es un proyecto artístico de mediación vecinal que propone una lectura de la ciudad de Madrid partiendo de los cuerpos que lo habitan, construyen y transforman, así como de sus memorias y cicatrices",
+    age: 19,
+  },
+  {
+    name: "Nadiner",
+    imageUrl:
+      "https://nadinefundacion.org/wp-content/uploads/2021/07/DSC04413.jpg",
+    description:
+      "Memento es un proyecto artístico de mediación vecinal que propone una lectura de la ciudad de Madrid partiendo de los cuerpos que lo habitan, construyen y transforman, así como de sus memorias y cicatrices",
+    age: 18,
+  },
+  {
+    name: "Nadiner",
+    imageUrl:
+      "https://nadinefundacion.org/wp-content/uploads/2021/07/DSC04413.jpg",
+    description:
+      "Memento es un proyecto artístico de mediación vecinal que propone una lectura de la ciudad de Madrid partiendo de los cuerpos que lo habitan, construyen y transforman, así como de sus memorias y cicatrices",
+    age: 19,
+  },
+  {
+    name: "Nadiner",
+    imageUrl:
+      "https://nadinefundacion.org/wp-content/uploads/2021/07/DSC04413.jpg",
+    description:
+      "Memento es un proyecto artístico de mediación vecinal que propone una lectura de la ciudad de Madrid partiendo de los cuerpos que lo habitan, construyen y transforman, así como de sus memorias y cicatrices",
+    age: 18,
+  },
+  {
+    name: "Nadiner",
+    imageUrl:
+      "https://nadinefundacion.org/wp-content/uploads/2021/07/DSC04413.jpg",
+    description:
+      "Memento es un proyecto artístico de mediación vecinal que propone una lectura de la ciudad de Madrid partiendo de los cuerpos que lo habitan, construyen y transforman, así como de sus memorias y cicatrices",
+    age: 19,
+  },
+  {
+    name: "Nadiner",
+    imageUrl:
+      "https://nadinefundacion.org/wp-content/uploads/2021/07/DSC04413.jpg",
+    description:
+      "Memento es un proyecto artístico de mediación vecinal que propone una lectura de la ciudad de Madrid partiendo de los cuerpos que lo habitan, construyen y transforman, así como de sus memorias y cicatrices",
+    age: 18,
+  },
+
+];
+
+module.exports =  artists;
